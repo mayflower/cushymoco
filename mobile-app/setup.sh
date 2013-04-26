4a04fac0348778f851f6054d970ac1a64cc20f12
Björn Richter <bjoern.richter@mayflower.de>
2013-04-26 20:30:46 +0200

    Moved app/config.cfg to .setup/app/
    * after copying config.json, setup script will ask for the base URL and set it in config.json
    * added setup specific files to .gitignore
    * added .tmp to .setup for temporary files while setup is running

--- mobile-app/setup.sh
+++ mobile-app/setup.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+defaultBaseURL="http://oxid.openstack/cl=cushymoco"
+
+# Copy files to their desired place
 cp -iv ./.setup/tiapp.xml ./
 cp -iv ./.setup/manifest ./
+cp -iv ./.setup/app/config.json ./app/
 
+if [ $? == 0 ]; then
+	# Get tht base URL the app should use (this will be inserted into 'app/config.json')
+	read -p "Please enter base URL [${defaultBaseURL}]: " baseURL
+	
+	# Use a default value if no base URL was entered
+	baseURL=${baseURL:-$defaultBaseURL}
+	
+	# Escape URL for use with sed
+	escapedBaseURL="${baseURL//\//\\/}"
+	
+	echo "Setting base URL to '${baseURL}' in app/config.json"
+	sed "s/\#{baseURL}/${escapedBaseURL}/g" ./app/config.json > ./.setup/.tmp/config.json
+	mv -f ./.setup/.tmp/config.json ./app/
+fi
+
+exit 0
