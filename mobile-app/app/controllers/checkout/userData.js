a3a2bfe1880d51d1fcee0d83389de01540aaa8db
Stefan Krenz <stefan.krenz@mayflower.de>
2013-05-17 13:18:38 +0200

    Added user data page in checkout process.

---
+++ mobile-app/app/controllers/checkout/userData.js
@@ -0,0 +1,32 @@
+var args = arguments[0] || {};
+
+var userDetails = Alloy.createModel('userDetails');
+userDetails.fetch();
+userDetails.on('change', function() {
+    $.accountUsername.text = userDetails.account.get('firstname') + ' ' + userDetails.account.get('lastname');
+    $.accountEmail.text = userDetails.account.get('username');
+    $.accountCompany.text = userDetails.account.get('company');
+    
+    $.billingStreet.text = userDetails.billingAddress.get('street') + ' ' + userDetails.billingAddress.get('streetNo');
+    $.billingStreetAdd.text = userDetails.billingAddress.get('additional');
+    $.billingZipCity.text = userDetails.billingAddress.get('zip') + ' ' + userDetails.billingAddress.get('city');
+    $.billingState.text = userDetails.billingAddress.get('state');
+    $.billingCountry.text = userDetails.billingAddress.get('country');
+    if (!userDetails.billingAddress.get('state') || userDetails.billingAddress.get('state').length == 0) {
+        $.billingState.visible = false;
+    }
+    if (!userDetails.billingAddress.get('additional') || userDetails.billingAddress.get('additional').length == 0) {
+        $.billingStreetAdd.visible = false;
+    }
+});
+
+var nextStepButton = Ti.UI.createButton({
+    title:"Weiter >"
+});
+nextStepButton.addEventListener('click', function(e) {
+    var shippingPaymentWin = Ti.UI.createWindow({
+        title:"Ramses Pyramiden Verleih"
+    });
+    args.nav.open(shippingPaymentWin);
+});
+$.userDataWindow.rightNavButton = nextStepButton;
